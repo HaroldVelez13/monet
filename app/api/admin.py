3df49a388c66364edd088bb1589eab56ef12459e
Harold Velez <haroldvelez113@gmail.com>
2023-02-26 18:14:53 -0500

    fix: Models, and add to admin panel

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from api.models import (Student, Teacher, Test, Question, Answer, User)
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    readonly_fields = ['user']
+    def get_list_display(self, request):
+        return ['name',]
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    readonly_fields = ['user']
+    def get_list_display(self, request):
+        return ['name',]
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
--- app/api/models.py
+++ app/api/models.py
@@ -11,22 +11,32 @@ class User(AbstractUser):
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
 
     def __str__(self):
         return f"{self.user.username}"
 
     def save(self, *args, **kwargs):
-        self.is_student = True
+        if self.pk is None:
+            user = User.objects.create_user(
+                username=self.name, password=self.password, is_student = True)
+            self.user = user
         super(Student, self).save(*args, **kwargs)
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
 
     def __str__(self):
         return f"{self.user.username}"
 
     def save(self, *args, **kwargs):
-        self.is_teacher = True
+        if self.pk is None:
+            user = User.objects.create_user(
+                username=self.name, password=self.password, is_teacher = True)
+            self.user = user
         super(Teacher, self).save(*args, **kwargs)
 
 
@@ -35,22 +45,21 @@ class Test(models.Model):
     description = models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.description}"
 
 
 OPTION_TYPE_CHOICES = [
-        ('A', 0),
-        ('B', 1),
-        ('C', 2),
-        ('D', 3),
+        (0, 'A'),
+        (1, 'B'),
+        (2, 'C'),
+        (3, 'D'),
     ]
 
 class Question(models.Model):  
     test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='questions')
     statement = models.CharField(max_length=200)
     options = ArrayField( models.CharField(max_length=200), size=4 )
-    correct_option = models.CharField(
-        max_length=1,
+    correct_option = models.IntegerField(
         choices=OPTION_TYPE_CHOICES
     )  
 
@@ -61,8 +70,7 @@ class Question(models.Model):
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='students')
-    selected_option = models.CharField(
-        max_length=1,
+    selected_option = models.IntegerField(
         choices=OPTION_TYPE_CHOICES
     )
 
